81634a0f8080afb6d94ea80ae961a9370e2907bb
Sean Velasco <seanvelasco@ieee.org>
2022-05-01 16:30:35 +0800

    Added more social links and respective shadows

--- app/root.tsx
+++ app/root.tsx
@@ -26,28 +26,26 @@ export default function App() {
 			</head>
 			<Global styles={
 				css`
-				* {
-  box-sizing: border-box;
-}
-					body {
-						background: rgb(25, 25, 25) none repeat scroll 0% 0%;
-						color: rgba(255, 255, 255, 0.81);
-						font-family: ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol";
-						width: 100vw;
-						height: 100%;
-						position: relative;
-						display: flex;
-						margin: 0;
-						padding: 0;
-					}
-					h1, h2, h3 {
-						margin: 0;
-					}
 
-
-				`
+				*{
+  					box-sizing: border-box;
 				}
-			/>
+				body {
+					background: rgb(25, 25, 25) none repeat scroll 0% 0%;
+					color: rgba(255, 255, 255, 0.81);
+					font-family: ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, "Apple Color Emoji", Arial, sans-serif, "Segoe UI Emoji", "Segoe UI Symbol";
+					width: 100vw;
+					height: 100%;
+					position: relative;
+					display: flex;
+					margin: 0;
+					padding: 0;
+				}
+				h1, h2, h3 {
+					margin: 0;
+				}
+				`
+			}/>
 			<body>
 				<Outlet />
 				<ScrollRestoration />
